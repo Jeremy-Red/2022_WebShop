eda8d0635f21ff800b8e5e737fe12e19a4161c00
Jeremy-red <jeremy.red.1991@gmail.com>
2024-05-11 20:22:45 +0300

    feat: config FW. Set constant, connect autoload

--- www/public/index.php
+++ www/public/index.php
@@ -1,8 +1,5 @@
 <?php
-echo __FILE__;
-echo '<br>';
-print_r($_GET);
-echo '<br>';
-echo $_SERVER['QUERY_STRING'];
-echo '<br>';
-echo $_SERVER['REQUEST_URI'];
+if (PHP_MAJOR_VERSION < 8) {
+    die('PHP version less than 8 is not supported');
+}
+require_once dirname(__DIR__) . '/config/init.php';

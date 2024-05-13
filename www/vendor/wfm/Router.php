c0192ecc6210b8b371315fb6f067a0dd2906924e
Jeremy-red <jeremy.red.1991@gmail.com>
2024-05-13 19:45:05 +0300

    feat: [Creating Router part 2] matchRoutes into Router

--- www/vendor/wfm/Router.php
+++ www/vendor/wfm/Router.php
@@ -19,7 +19,43 @@ class Router
     }
     public static function dispatch($url)
     {
-        var_dump($url);
-        debug(self::$routes);
+        if (self::matchRoute($url)) {
+        } else {
+        }
+    }
+    public static function matchRoute($url): bool
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#", $url, $matches)) {
+                debug($route);
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                if (empty($route['action']))
+                    $route['action'] = 'index';
+                if (!isset($route['admin_prefix']))
+                    $route['admin_prefix'] = '';
+                else
+                    $route['admin_prefix'] = '\\';
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                debug($route);
+                return true;
+            }
+        }
+        return false;
+    }
+    protected static function upperCamelCase($name): string
+    {
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
+    protected static function lowerCamelCase($name): string
+    {
+        $name = lcfirst(self::upperCamelCase($name));
+        return $name;
     }
 }

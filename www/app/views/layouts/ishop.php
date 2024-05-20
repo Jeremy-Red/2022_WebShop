a35c65590456f64b9fd02ca696e17cc81c38309d
Jeremy-red <jeremy.red.1991@gmail.com>
2024-05-20 14:17:20 +0300

    feat: [Transfering layout from index.html] complete

--- www/app/views/layouts/ishop.php
+++ www/app/views/layouts/ishop.php
@@ -4,9 +4,5 @@ use wfm\View;
 /** @var View $this */
 ?>
 <?php $this->getPart('parts/header'); ?>
-
-<p>This is default layout ishop.php</p>
-
 <?= $this->content; ?>
-
 <?php $this->getPart('parts/footer'); ?>

8066d6bace76520c2a6f9012e27a7e23b4531dc9
Jeremy-red <jeremy.red.1991@gmail.com>
2024-05-20 19:59:19 +0300

    feat: [slider realized] complete

--- www/app/controllers/MainController.php
+++ www/app/controllers/MainController.php
@@ -2,12 +2,17 @@
 namespace app\controllers;
 
 use app\models\Main;
+use RedBeanPHP\R;
+
+
 
 /** @property Main $model */
 class MainController extends AppController
 {
     public function indexAction()
     {
-
+        $slides = R::findAll('slider');
+        // debug(count($slides), true);
+        $this->set(compact('slides'));
     }
 }

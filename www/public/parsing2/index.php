5dc86fe23d409e73191386b9160a1397aec30ee5
Jeremy-red <jeremy.red.1991@gmail.com>
2024-05-31 08:27:21 +0300

    feat: remove parsers

--- www/public/parsing2/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use parsing\Parser;
-
-// $url = 'https://www.discudemy.com/feed/';
-// $url = 'https://www.noexistsrsslink.com/feed/';
-$url = './assets/test';
-require_once './Parser.php';
-$p = new Parser($url);
-$p->start();
